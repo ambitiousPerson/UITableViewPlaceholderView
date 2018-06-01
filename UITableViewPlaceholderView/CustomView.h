b57b51d656c16badf74202a5074cade8381df696
cuizhiwei <czwaizbq@163.com>
2018-06-01 16:06:15 +0800

    添加

---
+++ UITableViewPlaceholderView/CustomView.h
@@ -0,0 +1,13 @@
+//
+//  CustomView.h
+//  UITableViewPlaceholderView
+//
+//  Created by 崔志伟 on 2018/6/1.
+//  Copyright © 2018年 崔志伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+
+@end
---
+++ UITableViewPlaceholderView/CustomView.m
@@ -0,0 +1,21 @@
+//
+//  CustomView.m
+//  UITableViewPlaceholderView
+//
+//  Created by 崔志伟 on 2018/6/1.
+//  Copyright © 2018年 崔志伟. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
